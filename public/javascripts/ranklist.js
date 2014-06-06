c5c4caa67331c214d62f3f2c0acb2ed048869bfa
kidx <972169909@qq.com>
2014-06-06 11:52:29 +0800

    ranklist.js代码规范化

--- public/javascripts/ranklist.js
+++ public/javascripts/ranklist.js
@@ -1,49 +1,47 @@
-
-var cid = $('#ranklist').attr('cid')
-,	$fil = $('#fil')
-,	$search = $('#search')
-,	$list = $('#list').find('a');
+var cid = $('#ranklist').attr('cid');
+var $fil = $('#fil');
+var $search = $('#search');
+var $list = $('#list').find('a');
 
 function go(page){
-	var F = new Array(), G = new Array()
+  var F = new Array(), G = new Array()
 
-	if (page) F.push('page'), G.push(page);
-	if (cid) F.push('cid'), G.push(cid);
+  if (page) F.push('page'), G.push(page);
+  if (cid) F.push('cid'), G.push(cid);
 
-	if ($search.length) {
-		var search = JudgeString($search.val());
-		if (search) F.push('search'), G.push(search);
-	}
+  if ($search.length) {
+    var search = JudgeString($search.val());
+    if (search) F.push('search'), G.push(search);
+  }
 
-	var url = '/ranklist', flg = true;
-	for (var i = 0; i < F.length; i++) {
-		if (flg) {
-			url += '?';
-			flg = false;
-		} else {
-			url += '&';
-		}
-		url += F[i] + '=' + G[i];
-	}
+  var url = '/ranklist', flg = true;
+  for (var i = 0; i < F.length; i++) {
+    if (flg) {
+      url += '?';
+      flg = false;
+    } else {
+      url += '&';
+    }
+    url += F[i] + '=' + G[i];
+  }
 
-	window.location.href = url;
+  window.location.href = url;
 }
 
 $(document).ready(function(){
-	console.log(cid);
-	$.each($list, function(i, p){
-		$(p).click(function(){
-			var $li = $(this).parent();
-			if ($li.hasClass('active') || $li.hasClass('disabled')) {
-				return false;
-			}
-			go($(this).attr('id'));
-		});
-	});
-	if ($fil.length) {
-		$fil.click(function(){
-			go(null);
-		});
-		simulateClick($search, $fil);
-	}
+  $.each($list, function(i, p){
+    $(p).click(function(){
+      var $li = $(this).parent();
+      if ($li.hasClass('active') || $li.hasClass('disabled')) {
+        return false;
+      }
+      go($(this).attr('id'));
+    });
+  });
+  if ($fil.length) {
+    $fil.click(function(){
+      go(null);
+    });
+    simulateClick($search, $fil);
+  }
 });
