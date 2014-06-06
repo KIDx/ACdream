15fd575c9d0d996d80a8924b5b26e373e365c2ab
kidx <972169909@qq.com>
2014-06-06 11:55:06 +0800

    statistic.js代码规范化

--- public/javascripts/statistic.js
+++ public/javascripts/statistic.js
@@ -1,83 +1,82 @@
-
-var $lang = $('#lang')
-,   $list = $('#list').find('a')
-,   $sort = $('a.sort')
-,   $statistic = $('#statistic')
-,   pid = $statistic.attr('pid')
-,   sort = $statistic.attr('sort');
+var $lang = $('#lang');
+var $list = $('#list').find('a');
+var $sort = $('a.sort');
+var $statistic = $('#statistic');
+var pid = $statistic.attr('pid');
+var sort = $statistic.attr('sort');
 
 function go(page){
-	var F = new Array(), G = new Array()
-	,	lang = $lang.val();
+  var F = new Array(), G = new Array();
+  var lang = $lang.val();
 
-	if (page) F.push('page'), G.push(page);
-	if (lang) F.push('lang'), G.push(lang);
-	if (sort) F.push('sort'), G.push(sort);
+  if (page) F.push('page'), G.push(page);
+  if (lang) F.push('lang'), G.push(lang);
+  if (sort) F.push('sort'), G.push(sort);
 
-	var url = '/statistic/'+pid, flg = true;
-	for (var i = 0; i < F.length; i++) {
-		if (flg) {
-			url += '?';
-			flg = false;
-		} else {
-			url += '&';
-		}
-		url += F[i] + '=' + G[i];
-	}
+  var url = '/statistic/'+pid, flg = true;
+  for (var i = 0; i < F.length; i++) {
+    if (flg) {
+      url += '?';
+      flg = false;
+    } else {
+      url += '&';
+    }
+    url += F[i] + '=' + G[i];
+  }
 
-	window.location = url;
+  window.location = url;
 }
 
 $(document).ready(function(){
-	$.each($list, function(i, p){
-		$(p).click(function(){
-			var $li = $(this).parent();
-			if ($li.hasClass('active') || $li.hasClass('disabled')) {
-				return false;
-			}
-			go($(this).attr('id'));
-		});
-	});
-	$.each($sort, function(i, p){
-		$(p).click(function(){
-			sort = i;
-			go(null);
-		});
-	});
-	$lang.change(function(){
-		go(null);
-	});
+  $.each($list, function(i, p){
+    $(p).click(function(){
+      var $li = $(this).parent();
+      if ($li.hasClass('active') || $li.hasClass('disabled')) {
+        return false;
+      }
+      go($(this).attr('id'));
+    });
+  });
+  $.each($sort, function(i, p){
+    $(p).click(function(){
+      sort = i;
+      go(null);
+    });
+  });
+  $lang.change(function(){
+    go(null);
+  });
 });
 
 $(document).ready(function(){
-	$('#chart').highcharts({
-		chart: {
-			plotBackgroundColor: null,
-			plotBorderWidth: null,
-			plotShadow: false
-		},
-		credits: {
-			enabled: false
-		},
-		title: {
-			text: 'Statistic'
-		},
-		tooltip: {
-			pointFormat: '<b>{point.title}:{point.y}次</b><b>, {point.percentage:.1f}%</b>'
-		},
-		plotOptions: {
-			pie: {
-				allowPointSelect: true,
-				cursor: 'pointer',
-				dataLabels: {
-					enabled: true
-				},
-				showInLegend: true
-			}
-		},
-		series: [{
-			type: 'pie',
-			data: stat_data
-		}]
-	});
+  $('#chart').highcharts({
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false
+    },
+    credits: {
+      enabled: false
+    },
+    title: {
+      text: 'Statistic'
+    },
+    tooltip: {
+      pointFormat: '<b>{point.title}:{point.y}次</b><b>, {point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true
+        },
+        showInLegend: true
+      }
+    },
+    series: [{
+      type: 'pie',
+      data: stat_data
+    }]
+  });
 });
