3f189509fe1a6a4de597b36dc0af2364a87d5ee9
kidx <972169909@qq.com>
2014-06-06 11:56:14 +0800

    status.js代码规范化

--- public/javascripts/status.js
+++ public/javascripts/status.js
@@ -1,64 +1,63 @@
-
 $CE = $('.CE');
 $(document).ready(function(){
-	BindCE();
+  BindCE();
 });
 
-var $fil = $('#fil')
-,	$reset = $('#reset')
-,	$name = $('#name')
-,	$pid = $('#pid')
-,	$result = $('#result')
-,	$lang = $('#lang')
-,	$list = $('#list').find('a');
+var $fil = $('#fil');
+var $reset = $('#reset');
+var $name = $('#name');
+var $pid = $('#pid');
+var $result = $('#result');
+var $lang = $('#lang');
+var $list = $('#list').find('a');
 
 function go(page){
-	var F = new Array(), G = new Array()
-	,	name = JudgeString($name.val())
-	,	pid = parseInt($pid.val(), 10)
-	,	result = JudgeString($result.val())
-	,	lang = JudgeString($lang.val());
+  var F = new Array(), G = new Array();
+  var name = JudgeString($name.val());
+  var pid = parseInt($pid.val(), 10);
+  var result = JudgeString($result.val());
+  var lang = JudgeString($lang.val());
 
-	if (page) F.push('page'), G.push(page);
-	if (name) F.push('name'), G.push(name);
-	if (pid) F.push('pid'), G.push(pid);
-	if (result) F.push('result'), G.push(result);
-	if (lang) F.push('lang'), G.push(lang);
+  if (page) F.push('page'), G.push(page);
+  if (name) F.push('name'), G.push(name);
+  if (pid) F.push('pid'), G.push(pid);
+  if (result) F.push('result'), G.push(result);
+  if (lang) F.push('lang'), G.push(lang);
 
-	var url = '/status', flg = true;
-	for (var i = 0; i < F.length; i++) {
-		if (flg) {
-			url += '?';
-			flg = false;
-		} else {
-			url += '&';
-		}
-		url += F[i] + '=' + G[i];
-	}
+  var url = '/status', flg = true;
+  for (var i = 0; i < F.length; i++) {
+    if (flg) {
+      url += '?';
+      flg = false;
+    } else {
+      url += '&';
+    }
+    url += F[i] + '=' + G[i];
+  }
 
-	window.location.href = url;
+  window.location.href = url;
 }
 
 $(document).ready(function(){
-	$.each($list, function(i, p){
-		$(p).click(function(){
-			var $li = $(this).parent();
-			if ($li.hasClass('active') || $li.hasClass('disabled')) {
-				return false;
-			}
-			go($(this).attr('id'));
-		});
-	});
-	$fil.click(function(){
-		go(null);
-	});
-	$reset.click(function(){
-		window.location.href = '/status';
-	});
-	simulateClick($name, $fil);
-	simulateClick($pid, $fil);
+  $.each($list, function(i, p){
+    $(p).click(function(){
+      var $li = $(this).parent();
+      if ($li.hasClass('active') || $li.hasClass('disabled')) {
+        return false;
+      }
+      go($(this).attr('id'));
+    });
+  });
+  $fil.click(function(){
+    go(null);
+  });
+  $reset.click(function(){
+    window.location.href = '/status';
+  });
+  simulateClick($name, $fil);
+  simulateClick($pid, $fil);
 });
 
 $(document).ready(function(){
-	getStatus();
+  getStatus();
 });
