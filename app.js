9d234874292202399ed058fe8321a0b16c5bf118
kidx <972169909@qq.com>
2014-07-05 20:17:47 +0800

    remove method-override

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(require('multer')({
 }));
 
 app.use(require('compression')()); //gzip压缩传输
-app.use(require('method-override')());
 app.use(session({
   secret: settings.cookie_secret,
   store: sessionStore,
